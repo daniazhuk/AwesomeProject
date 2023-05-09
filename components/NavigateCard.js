53e06dd57910f6aa292bf44efdf773fdc4f07c7b
daniazhuk <danilzhuchenko@gmail.com>
2023-05-09 22:11:04 +0300

    created full rideoptionscard and travel time calculation

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {SafeAreaView, Text, View, StyleSheet} from "react-native";
+import {SafeAreaView, Text, View, StyleSheet, TouchableOpacity} from "react-native";
 import tw from "twrnc";
 import {GOOGLE_MAPS_APIKEY} from '@env'
 import {GooglePlacesAutocomplete} from "react-native-google-places-autocomplete";
@@ -7,15 +7,18 @@ import {setDestination} from "../slices/navSlice";
 import {useDispatch} from "react-redux";
 import {useNavigation} from "@react-navigation/native";
 import {KeyboardAwareScrollView} from "react-native-keyboard-aware-scrollview";
+import NavFavourites from "./NavFavourites";
+import {Icon} from "react-native-elements";
 
 const NavigateCard = () => {
-    const dispatch =  useDispatch();
+    const dispatch = useDispatch();
     const navigation = useNavigation();
 
     return (
-            <SafeAreaView style={tw`bg-white flex-1`}>
-                <Text style={tw`text-center py-5 text-xl`}>Good morning, Daniel!</Text>
-                <View style={tw`border-t border-gray-200 flex-shrink`}>
+        <SafeAreaView style={tw`bg-white flex-1`}>
+            <Text style={tw`text-center py-5 text-xl`}>Good morning, Daniel!</Text>
+            <View style={tw`border-t border-gray-200 flex-shrink`}>
+                <View>
                     <GooglePlacesAutocomplete
                         placeholder='Where to?'
                         styles={inPutBoxStyles}
@@ -41,7 +44,34 @@ const NavigateCard = () => {
                         debounce={400}
                     />
                 </View>
-            </SafeAreaView>
+                <NavFavourites/>
+            </View>
+
+            <View style={tw`flex-row bg-white justify-evenly py-2 mt-auto border-t border-gray-100`}>
+                <TouchableOpacity
+                    onPress={() => {
+                        navigation.navigate('RideOptionsCard')
+                    }}
+                    style={tw`flex flex-row justify-between bg-black w-24 px-4 py-3 rounded-full`}
+                >
+                    <Icon
+                        name='car'
+                        type='font-awesome'
+                        color='white'
+                        size={16}/>
+                    <Text style={tw`text-white text-center`}>Rides</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={tw`flex flex-row justify-between w-24 px-4 py-3 rounded-full`}>
+                    <Icon
+                        name='fast-food-outline'
+                        type='ionicon'
+                        color='black'
+                        size={16}/>
+                    <Text style={tw`text-center`}>Eats</Text>
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
     );
 };
 
@@ -73,87 +103,3 @@ const inPutBoxStyles = StyleSheet.create({
         paddingBottom: 0,
     },
 })
-// import React, {useState} from 'react';
-// import { SafeAreaView, Text, View, StyleSheet, FlatList } from 'react-native';
-// import tw from 'twrnc';
-// import { GOOGLE_MAPS_APIKEY } from '@env';
-// import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
-// import { setDestination } from '../slices/navSlice';
-// import { useDispatch } from 'react-redux';
-// import { useNavigation } from '@react-navigation/native';
-// import NavFavourites from "./NavFavourites";
-//
-// const NavigateCard = () => {
-//     const dispatch = useDispatch();
-//     const navigation = useNavigation();
-//
-//     const renderHeader = () => (
-//         <View style={tw`border-t border-gray-200 flex-1`}>
-//             <GooglePlacesAutocomplete
-//                 placeholder='Where to?'
-//                 styles={inPutBoxStyles}
-//                 fetchDetails={true}
-//                 returnKeyType={'search'}
-//                 minLength={2}
-//                 keyboardShouldPersistTaps='handled'
-//                 onPress={(data, details = null) => {
-//                     dispatch(
-//                         setDestination({
-//                             location: details.geometry.location,
-//                             description: data.description,
-//                         })
-//                     );
-//                     navigation.navigate('RideOptionsCard');
-//                 }}
-//                 enablePoweredByContainer={false}
-//                 query={{
-//                     key: GOOGLE_MAPS_APIKEY,
-//                     language: 'ru',
-//                 }}
-//                 nearbyPlacesAPI='GooglePlacesSearch'
-//                 debounce={400}
-//             />
-//             <NavFavourites/>
-//         </View>
-//     );
-//
-//     return (
-//         <SafeAreaView style={tw`bg-white flex-1`}>
-//             <FlatList
-//                 data={[{ key: 'header' }]}
-//                 renderItem={renderHeader}
-//                 keyExtractor={(item) => item.key}
-//                 scrollEnabled={true}
-//                 keyboardShouldPersistTaps='handled'
-//             />
-//         </SafeAreaView>
-//     );
-// };
-//
-// export default NavigateCard;
-//
-// const ScrollViewStyles = StyleSheet.create({
-//     container: {
-//         backgroundColor: '#4c69a5',
-//         flex: 1,
-//         alignItems: 'center',
-//         justifyContent: 'center',
-//     },
-// });
-//
-// const inPutBoxStyles = StyleSheet.create({
-//     container: {
-//         backgroundColor: 'white',
-//         paddingTop: 20,
-//         flex: 0,
-//     },
-//     textInput: {
-//         backgroundColor: '#DDDDDF',
-//         borderRadius: 0,
-//         fontSize: 18,
-//     },
-//     textInputContainer: {
-//         paddingHorizontal: 20,
-//         paddingBottom: 0,
-//     },
-// });
